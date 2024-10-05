1db93015bd8c29e1d7442720e9e6ed2f0eacfc26
Davide Cose <daviddavide847@gmail.com>
2024-10-06 00:10:26 +0200

    Create Esercizio2.java

---
+++ Esercizi sulla programmazione orientata agli oggetti/Esercizio 2/Esercizio2.java
@@ -0,0 +1,19 @@
+package esercizio.pkg2;
+
+public class Esercizio2 {
+    
+    public static void main(String[] args) {
+        Student Daniele = new Student("Daniele", "Rossi", 2007, "3A", "IA");
+        
+        System.out.println(Daniele);
+        
+        Student Giacomo = new Student("", "Rossi", 2007, "3A", "IA");
+        
+        System.out.println(Giacomo);
+        
+        Student Malcom = new Student("Malcom", "Aprile", 2010, "3A", "IA");
+        
+        System.out.println(Malcom);
+    }
+    
+}
